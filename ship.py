abfa028c04ee512962e7792f6236558fc158457b
matifyx <daniel_86@gmx.net>
2025-01-03 20:48:26 +0100

    schnellere Schiffsbewegung

--- ship.py
+++ ship.py
@@ -6,6 +6,7 @@ class Ship():
     def __init__(self, ai_game):
         """Initialisiert das Schiff und setzt es auf die Startposition"""        
         self.screen = ai_game.screen
+        self.settings = ai_game.settings
         self.screen_rect = ai_game.screen.get_rect()
 
         # Lädt das Bild des Schiffes und ruft dessen umgebendes Rechteck ab.
@@ -15,16 +16,24 @@ class Ship():
         # Platziert jedes neue Schiff mittig am unteren Bildschirmrand.
         self.rect.midbottom = self.screen_rect.midbottom
 
+        # Speichert einen Fließkommawert für den exakten Schiffsmittelpunkt.
+        self.x = float(self.rect.x)
+
         # Movemant flag; , beginne mit einem unbewegtem Schiff.
         self.moving_right = False
         self.moving_left = False
 
     def update(self):
         """Aktualisiert die Position des Schiffs abhängig von Movement Flag"""
+        # Aktualisiert den Wert für den Mittelpunkt des Schiffs,
+        # nicht des Rechtecks.
         if self.moving_right:
-            self.rect.x += 1
+            self.x += self.settings.ship_speed
         if self.moving_left:
-            self.rect.x -= 1
+            self.x -= self.settings.ship_speed
+
+        # Aktualisiert des rect-Objekt auf der Grundlage von self.x
+        self.rect.x = self.x
 
     def blitme(self):
         """Platziert das Schiff auf seine aktuelle Position"""
