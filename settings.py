15218d19044ad2c34aac93bec6db7742d6a7e180
matifyx <daniel_86@gmx.net>
2025-01-03 17:31:32 +0100

    Modul settings erstellt

---
+++ settings.py
@@ -0,0 +1,9 @@
+class Settings():
+    """Eine Klasse zum Speichern aller Einstellungen für Alien Invasion."""
+
+    def __init__(self):
+        """Initialisiert die Spieleinstellungen"""
+        # Bildschirmeinstellungen
+        self.screen_width = 1200
+        self.screen_height = 800
+        self.bg_color = (230, 230, 230)
