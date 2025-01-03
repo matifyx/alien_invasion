1e78d1ea5eb5c1af5714db49844622def032bec1
matifyx <daniel_86@gmx.net>
2025-01-03 17:24:47 +0100

    Framerate und Hintergrundfarbe definiert

--- alien_invasion.py
+++ alien_invasion.py
@@ -6,10 +6,17 @@ class AlienInvasion:
     def __init__(self):
         """Initialisiert das Spiel und erstellt Spielresourcen"""
         pygame.init()
+        # Erstellen einer Instanz der Klasse 'clock'
+        # Damit wird die Uhr am Ende der While-Schleife in run_game()
+        # zum Ticken gebracht
+        self.clock = pygame.time.Clock()
         # Fenster indem alle grafischen Elemente erstellt werden
         # Übergabe eines Tupels -> Fenstergröße 1200 x 800 Pixel (B x H)
         self.screen = pygame.display.set_mode((1200, 800))
         pygame.display.set_caption("Alien Invasion")
+
+        # Legt die Hintergrundfarbe fest.
+        self.bg_color = (230, 230, 230)
 
     def run_game(self):
         """Start der Hauptschleife für das Spiel."""
@@ -19,8 +26,12 @@ class AlienInvasion:
                 if event.type == pygame.QUIT:
                     sys.exit()
 
+            # Zeichnet den Bildschirm bei jedem Schleifendurchlauf neu.
+            self.screen.fill(self.bg_color)
+
             # Macht den zuletzt gezeichneten Bildschirm sichtbar.
             pygame.display.flip()
+            self.clock.tick(60)
 
 if __name__ == '__main__':
     # Erstellt eine Spielinstanz und führt das Spiel aus.
